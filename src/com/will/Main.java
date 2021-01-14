ce89dcca1f955a056e3174d7dddf85c2355d27b7
Will brown <wtbrown10@gmail.com>
2021-01-14 17:29:15 -0500

    added finals

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -1,11 +1,15 @@
 package com.will;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        final byte MonthsInYears = 12;
+        final byte Percent = 100;
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
@@ -14,13 +18,15 @@ public class Main {
         System.out.print("Annual Interest Rate: ");
         float interest = scanner.nextFloat();
 //        System.out.println("Your interest rate is " + interest);
+        float monthlyInterest = interest / Percent / MonthsInYears;
 
         System.out.print("Period (Years): ");
         int period = scanner.nextInt();
 //        System.out.println("Your Period is " + period + " years!");
 
-        long mortgage = Math.round(principal * ((interest * (Math.pow((1 + interest), period))) / (Math.pow((1 + interest), period) - 1))) ;
+        long mortgage = Math.round(principal * ((monthlyInterest * (Math.pow((1 + monthlyInterest), period))) / (Math.pow((1 + monthlyInterest), period) - 1))) ;
+        String mortgageFormated = NumberFormat.getCurrencyInstance().format(mortgage);
 
-        System.out.print("Mortgage: " + mortgage);
+        System.out.print("Mortgage: " + mortgageFormated);
     }
 }
